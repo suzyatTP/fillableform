99afc01bc36bc5cd17c38f93140955e534ba94af
Suzy Utterback <sutterback@CRE-LN29G6K14C.local>
2025-04-02 09:18:09 -0700

    text height top ection

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def submit():
         p.drawString(50, y, label)
         p.rect(50, y - box_height - 5, width - 100, box_height, stroke=1, fill=0)
         p.setFont("Helvetica-Bold", 12)
-        draw_wrapped_text(p, 55, y - 15, val, width - 110)
+        draw_wrapped_text(p, 55, y - 20, val, width - 110)
         y -= (box_height + 20)
 
     p.showPage()
